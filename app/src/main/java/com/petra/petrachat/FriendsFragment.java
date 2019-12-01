c2f70ed5d2acf161d356cb016088609e8d88df61
nichokevin <58069843+nichokevin@users.noreply.github.com>
2019-12-01 19:19:10 +0700

    part 26(blm selesai)

--- app/src/main/java/com/petra/petrachat/FriendsFragment.java
+++ app/src/main/java/com/petra/petrachat/FriendsFragment.java
@@ -4,17 +4,29 @@ package com.petra.petrachat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FriendsFragment extends Fragment {
+    private RecyclerView mFriendlist;
 
+    private DatabaseReference mFriendDatabase;
+    private FirebaseAuth mAuth;
+
+    private String mCurrent_user_id;
+    private View mMainView;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -24,8 +36,16 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mMainView = inflater.inflate(R.layout.fragment_friends,container,false);
+        mFriendlist = mMainView.findViewById(R.id.friend_list);
+        mAuth = FirebaseAuth.getInstance();
+
+        mCurrent_user_id = mAuth.getCurrentUser().getUid();
+        mFriendDatabase = FirebaseDatabase.getInstance().getReference().child("friends").child(mCurrent_user_id);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return mMainView;
     }
+
 
 }
