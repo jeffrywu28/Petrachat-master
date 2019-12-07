047ed0dca2475dc8d04b1a9cb01852c2261312a6
nichokevin <58069843+nichokevin@users.noreply.github.com>
2019-12-07 17:49:22 +0700

    part35 (kurang onStart)

--- app/src/main/java/com/petra/petrachat/ChatsFragment.java
+++ app/src/main/java/com/petra/petrachat/ChatsFragment.java
@@ -1,20 +1,133 @@
 package com.petra.petrachat;
 
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatsFragment extends Fragment {
+    private RecyclerView mConvList;
+
+    private DatabaseReference mConvDatabase;
+    private DatabaseReference mMessageDatabase;
+    private DatabaseReference mUsersDatabase;
+
+    private FirebaseAuth mAuth;
+
+    private String mCurrent_user_id;
+
+    private View mMainView;
+
+
+
 
     public ChatsFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        mMainView = inflater.inflate(R.layout.fragment_chats, container, false);
+        mConvList = mMainView.findViewById(R.id.conv_list);
+        mAuth = FirebaseAuth.getInstance();
+        mCurrent_user_id = mAuth.getCurrentUser().getUid();
+
+        mConvDatabase = FirebaseDatabase.getInstance().getReference().child("Chat").child(mCurrent_user_id);
+
+        mConvDatabase.keepSynced(true);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mMessageDatabase = FirebaseDatabase.getInstance().getReference().child("messages").child(mCurrent_user_id);
+        mUsersDatabase.keepSynced(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        mConvList.setHasFixedSize(true);
+        mConvList.setLayoutManager(linearLayoutManager);
+
+
+        // Inflate the layout for this fragment
+        return mMainView;
     }
 
+
+
+
+    public static  class  ConvViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public ConvViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView=itemView;
+        }
+        public void setMessage(String message, boolean isSeen){
+
+            TextView userStatusView = mView.findViewById(R.id.user_single_status);
+            userStatusView.setText(message);
+
+            if(!isSeen){
+                userStatusView.setTypeface(userStatusView.getTypeface(), Typeface.BOLD);
+            } else {
+                userStatusView.setTypeface(userStatusView.getTypeface(), Typeface.NORMAL);
+            }
+
+        }
+
+        public void setName(String name){
+
+            TextView userNameView = mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+
+        }
+
+        public void setUserImage(String thumb_image, Context ctx){
+
+            CircleImageView userImageView = mView.findViewById(R.id.user_single_image);
+            Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.defaultprofile).into(userImageView);
+
+        }
+
+        public void setUserOnline(String online_status) {
+
+            ImageView userOnlineView = mView.findViewById(R.id.user_single_online_icon);
+
+            if(online_status.equals("true")){
+
+                userOnlineView.setVisibility(View.VISIBLE);
+
+            } else {
+
+                userOnlineView.setVisibility(View.INVISIBLE);
+
+            }
+
+        }
+    }
 }
