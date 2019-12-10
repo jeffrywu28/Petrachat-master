73f1de91b237bd796270d3587a357c14cdb84d5e
Jeffry Gunawan <jefgu28@gmail.com>
2019-12-10 23:47:21 +0700

    35 (half)
    fixed error but still each fragment not showing anything

--- app/src/main/java/com/petra/petrachat/ChatsFragment.java
+++ app/src/main/java/com/petra/petrachat/ChatsFragment.java
@@ -6,9 +6,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,9 +34,12 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.Objects;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatsFragment extends Fragment {
+
     private RecyclerView mConvList;
 
     private DatabaseReference mConvDatabase;
@@ -41,21 +47,21 @@ public class ChatsFragment extends Fragment {
     private DatabaseReference mUsersDatabase;
 
     private FirebaseAuth mAuth;
-
     private String mCurrent_user_id;
 
     private View mMainView;
+    private Query query;
 
-
-
-
-    public ChatsFragment() { }
+    public ChatsFragment() {
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         mMainView = inflater.inflate(R.layout.fragment_chats, container, false);
         mConvList = mMainView.findViewById(R.id.conv_list);
         mAuth = FirebaseAuth.getInstance();
+
         mCurrent_user_id = mAuth.getCurrentUser().getUid();
 
         mConvDatabase = FirebaseDatabase.getInstance().getReference().child("Chat").child(mCurrent_user_id);
@@ -72,24 +78,128 @@ public class ChatsFragment extends Fragment {
         mConvList.setHasFixedSize(true);
         mConvList.setLayoutManager(linearLayoutManager);
 
-
-        // Inflate the layout for this fragment
         return mMainView;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Query conversationQuery = mConvDatabase.orderByChild("timestamp");
+
+        FirebaseRecyclerOptions<Conv> options2 = new FirebaseRecyclerOptions.Builder<Conv>()
+                .setQuery(conversationQuery, Conv.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        FirebaseRecyclerAdapter<Conv, ConvViewHolder> chatrecycleradapter = new FirebaseRecyclerAdapter<Conv, ConvViewHolder>(options2){
+            @NonNull
+            @Override
+            public ConvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_single_layout, parent, false);
+                return new ConvViewHolder(mView);
+            }
+
+            @Override
+            protected void onBindViewHolder(final ConvViewHolder convViewHolder, int position,final Conv conv) {
+                final String list_user_id = getRef(position).getKey();
+                Query lastMessageQuery = mMessageDatabase.child(list_user_id).limitToLast(1);
+
+                lastMessageQuery.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot,String s) {
+                        String data = dataSnapshot.child("message").getValue().toString();
+                        convViewHolder.setMessage(data, conv.isSeen());
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                mUsersDatabase.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        final String userName = dataSnapshot.child("name").getValue().toString();
+                        String userThumb = dataSnapshot.child("thumb_image").getValue().toString();
+
+                        if (dataSnapshot.hasChild("online")) {
+
+                            String userOnline = dataSnapshot.child("online").getValue().toString();
+                            convViewHolder.setUserOnline(userOnline);
+
+                        }
+
+                        convViewHolder.setName(userName);
+                        convViewHolder.setUserImage(userThumb, getContext());
+
+                        convViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
 
 
+                                Intent chatIntent = new Intent(getContext(), ChatActivity.class);
+                                chatIntent.putExtra("user_id", list_user_id);
+                                chatIntent.putExtra("user_name", userName);
+                                startActivity(chatIntent);
 
-    public static  class  ConvViewHolder extends RecyclerView.ViewHolder{
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        };
+        mConvList.setAdapter(chatrecycleradapter);
+    }
+
+    public static class ConvViewHolder extends RecyclerView.ViewHolder {
 
         View mView;
-        public ConvViewHolder(@NonNull View itemView) {
+
+        public ConvViewHolder(View itemView) {
+
             super(itemView);
-            mView=itemView;
+            mView = itemView;
+        }
+
+        public void setName(String name){
+
+            TextView userNameView = (TextView) mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+
+        }
+
+        public void setUserImage(String thumb_image, Context ctx){
+
+            CircleImageView userImageView = (CircleImageView) mView.findViewById(R.id.user_single_image);
+            Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.defaultprofile).into(userImageView);
+
         }
         public void setMessage(String message, boolean isSeen){
 
-            TextView userStatusView = mView.findViewById(R.id.user_single_status);
+            TextView userStatusView = (TextView) mView.findViewById(R.id.user_single_status);
             userStatusView.setText(message);
 
             if(!isSeen){
@@ -100,23 +210,9 @@ public class ChatsFragment extends Fragment {
 
         }
 
-        public void setName(String name){
-
-            TextView userNameView = mView.findViewById(R.id.user_single_name);
-            userNameView.setText(name);
-
-        }
-
-        public void setUserImage(String thumb_image, Context ctx){
-
-            CircleImageView userImageView = mView.findViewById(R.id.user_single_image);
-            Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.defaultprofile).into(userImageView);
-
-        }
-
         public void setUserOnline(String online_status) {
 
-            ImageView userOnlineView = mView.findViewById(R.id.user_single_online_icon);
+            ImageView userOnlineView = (ImageView) mView.findViewById(R.id.user_single_online_icon);
 
             if(online_status.equals("true")){
 
@@ -129,5 +225,10 @@ public class ChatsFragment extends Fragment {
             }
 
         }
+
+
     }
+
+
+
 }
